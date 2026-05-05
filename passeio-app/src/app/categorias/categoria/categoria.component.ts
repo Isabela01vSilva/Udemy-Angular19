75f6fe1a00c3bbdb9477991705f400bfd25518ce
isabela.estudosv067@gmail.com <isabela.estudosv067@gmail.com>
2026-05-05 07:55:21 -0300

    feat: implement reactive form for Categoria component with validation

--- passeio-app/src/app/categorias/categoria/categoria.component.ts
+++ passeio-app/src/app/categorias/categoria/categoria.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-categoria',
-  templateUrl: './categoria.component.html',
-  styleUrl: './categoria.component.scss'
+  templateUrl: './categoria.component.html'
 })
 export class CategoriaComponent {
+  camposForms: FormGroup;
 
+  constructor() { 
+    this.camposForms = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      descricao: new FormControl('', Validators.required),
+    });
+  }
+
+  salvar(){
+    this.camposForms.markAllAsTouched();
+
+    if(this.camposForms.invalid){
+      console.log(this.camposForms.value);
+    }
+
+    console.log("Ta valido?", this.camposForms.valid);
+  }
+
+  isCampoInvalido(nomeCampo: string): boolean{
+    const campo = this.camposForms.get(nomeCampo);
+    return campo?.invalid && campo.touched && campo?.errors?.['required'] || false;
+  }
 }
