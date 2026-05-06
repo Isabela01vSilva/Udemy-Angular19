3e2a4940ce5c543187abb488abb4009cb8f2f67c
Isabela Silva <isabela.estudosv067@gmail.com>
2026-05-06 17:03:39 -0300

    feat: implement LugarComponent with OnInit lifecycle and category fetching

--- passeio-app/src/app/lugares/lugar/lugar.component.ts
+++ passeio-app/src/app/lugares/lugar/lugar.component.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Categoria } from '../../categorias/categoria';
+import { CategoriaService } from '../../categorias/categoria.service';
 
 @Component({
   selector: 'app-lugar',
   templateUrl: './lugar.component.html',
   styleUrl: './lugar.component.scss',
 })
-export class LugarComponent {
+export class LugarComponent implements OnInit {
   camposForms: FormGroup;
   categorias: Categoria[] = [];
 
-  constructor() {
+  constructor(private categoriaService: CategoriaService) {
     this.camposForms = new FormGroup({
       nome: new FormControl('', Validators.required),
       descricao: new FormControl('', Validators.required),
@@ -21,13 +22,22 @@ export class LugarComponent {
       avaliacao: new FormControl('', Validators.required),
     });
   }
+  ngOnInit(): void {
+    this.categoriaService.obterTodas().subscribe({
+      next: (listaCategorias) => {
+        this.categorias = listaCategorias;
+      },
+    });
+  }
 
-  salvar(){
+  salvar() {
     console.log(this.camposForms.value);
   }
 
-  isCampoInvalido(nomeCampo: string): boolean{
+  isCampoInvalido(nomeCampo: string): boolean {
     const campo = this.camposForms.get(nomeCampo);
-    return campo?.invalid && campo.touched && campo?.errors?.['required'] || false;
+    return (
+      (campo?.invalid && campo.touched && campo?.errors?.['required']) || false
+    );
   }
 }
