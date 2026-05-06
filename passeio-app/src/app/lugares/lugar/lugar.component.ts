5b119e0f2832eb4619cf8a1aff4f091fc641c88d
Isabela Silva <isabela.estudosv067@gmail.com>
2026-05-06 16:55:20 -0300

    feat: implement Lugar component with reactive form for location management

--- passeio-app/src/app/lugares/lugar/lugar.component.ts
+++ passeio-app/src/app/lugares/lugar/lugar.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Categoria } from '../../categorias/categoria';
 
 @Component({
   selector: 'app-lugar',
   templateUrl: './lugar.component.html',
-  styleUrl: './lugar.component.scss'
+  styleUrl: './lugar.component.scss',
 })
 export class LugarComponent {
+  camposForms: FormGroup;
+  categorias: Categoria[] = [];
 
+  constructor() {
+    this.camposForms = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      descricao: new FormControl('', Validators.required),
+      categoria: new FormControl('', Validators.required),
+      localizacao: new FormControl('', Validators.required),
+      urlImagem: new FormControl('', Validators.required),
+      avaliacao: new FormControl('', Validators.required),
+    });
+  }
+
+  salvar(){
+    console.log(this.camposForms.value);
+  }
+
+  isCampoInvalido(nomeCampo: string): boolean{
+    const campo = this.camposForms.get(nomeCampo);
+    return campo?.invalid && campo.touched && campo?.errors?.['required'] || false;
+  }
 }
