e241096284917c8e55638cc5064de4224bd48329
Isabela Silva <isabela.estudosv067@gmail.com>
2026-05-07 15:35:55 -0300

    feat: implement GaleriaComponent with filtering and dynamic lugar listing

--- passeio-app/src/app/galeria/galeria/galeria.component.ts
+++ passeio-app/src/app/galeria/galeria/galeria.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Lugar } from '../../lugares/lugar';
+import { Categoria } from '../../categorias/categoria';
+import { CategoriaService } from '../../categorias/categoria.service';
+import { LugarService } from '../../lugares/lugar.service';
 
 @Component({
   selector: 'app-galeria',
   templateUrl: './galeria.component.html',
-  styleUrl: './galeria.component.scss'
+  styleUrl: './galeria.component.scss',
 })
-export class GaleriaComponent {
+export class GaleriaComponent implements OnInit {
+  lugares: Lugar[] = [];
+  categoriasFiltro: Categoria[] = [];
 
+  constructor(
+    private lugarService: LugarService,
+    private categoriaService: CategoriaService,
+  ) {}
+
+  ngOnInit(): void {
+    this.categoriaService.obterTodas().subscribe({
+      next: (listaCategorias) => {
+        this.categoriasFiltro = listaCategorias;
+      },
+    });
+
+    this.lugarService.obterLugares().subscribe({
+      next: (listaLugares) => {
+        this.lugares = listaLugares;
+      },
+    });
+  }
+
+  getTotalEstrelas(lugar: Lugar): string {
+    return '&#9733;'.repeat(lugar.avaliacao || 0) + '&#9734;'.repeat(5 - (lugar.avaliacao || 0));
+  }
 }
