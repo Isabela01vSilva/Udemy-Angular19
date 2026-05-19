7775dd96343290b7a57219ca06c2b8a6cd187d26
Isabela01vSilva <isabela.estudosv067@gmail.com>
2026-05-19 15:37:05 -0300

    feat: reestruturar módulo de formulários, remover componente DataForm e atualizar rotas

--- loiane-angular-16/forms/src/app/app.module.ts
+++ loiane-angular-16/forms/src/app/app.module.ts
@@ -1,27 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { DataFormComponent } from './data-form/data-form.component';
 import { TemplateFormModule } from './template-form/template-form.module';
+import { DataFormModule } from './data-form/data-form.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DataFormComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
-    TemplateFormModule
+    ReactiveFormsModule,
+    TemplateFormModule,
+    DataFormModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
