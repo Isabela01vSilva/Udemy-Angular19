8c08a4d836e2e6b205f9e6df8464640fbcbfa616
Isabela Silva <isabela.estudosv067@gmail.com>
2026-05-19 11:17:08 -0300

    feat: add CEP consultation functionality and integrate HTTP client for address autofill

--- loiane-angular-16/forms/src/app/template-form/template-form.component.ts
+++ loiane-angular-16/forms/src/app/template-form/template-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/internal/operators/map';
 
 @Component({
   selector: 'app-template-form',
@@ -6,21 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./template-form.component.css'],
 })
 export class TemplateFormComponent implements OnInit {
-
   usuario: any = {
     nome: null,
-    email: null
-  }
+    email: null,
+  };
 
-  onSubmit(form){
+  onSubmit(form) {
     console.log(form);
-
   }
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   verificaValidTouched(campo) {
     return !campo.valid && campo.touched;
@@ -29,7 +27,59 @@ export class TemplateFormComponent implements OnInit {
   aplicaCssErro(campo) {
     return {
       'has-error': this.verificaValidTouched(campo),
-      'has-feedback': this.verificaValidTouched(campo)
+      'has-feedback': this.verificaValidTouched(campo),
+    };
+  }
+
+  consultaCEP(cep, form) {
+    //Nova variável "cep" somente com dígitos.
+    cep = cep.replace(/\D/g, '');
+
+    //Verifica se campo cep possui valor informado.
+    if (cep != '') {
+
+      this.resetarDados(form);
+
+      //Expressão regular para validar o CEP.
+      var validacep = /^[0-9]{8}$/;
+
+      //Valida o formato do CEP.
+      if (validacep.test(cep)) {
+        this.http
+          .get(`https://viacep.com.br/ws/${cep}/json/`)
+          .subscribe((dados) => this.populaDadosForm(dados, form));
+      }
     }
+  }
+
+  populaDadosForm(dados, formulario) {
+    /* formulario.setValue({
+      nome: formulario.value.nome,
+      email: formulario.value.email,
+      endereco: {
+        cep: dados.cep,
+        rua: dados.logradouro,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf,
+        numero: '',
+        complemento: '',
+      },
+    }); */
+
+    formulario.form.patchValue({
+      endereco: {
+        rua: dados.logradouro,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf,
+      },
+    });
+
+    console.log(formulario);
+  }
+
+  resetarDados(formulario) {
+    formulario.reset();
   }
 }
